9efcf15d9f7da5dfb5a1cb9be66aed40db363b76
Michael <michaeljohn_charles@yahoo.com>
2024-09-28 11:21:09 +0100

    expo app start with screens

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import  ModuleListScreen  from '../crudler/src/components/screens/ModuleListScreen';
+import ModuleAddScreen from '../crudler/src/components/screens/ModuleAddScreen';
+import ModuleViewScreen from '../crudler/src/components/screens/ModuleViewScreen';
+import ModuleModifyScreen from '../crudler/src/components/screens/ModuleModifyScreen';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='ModuleListScreen'>
+        <Stack.Screen name='ModuleListScreen' component={ModuleListScreen} options={{title: 'List Modules'}}>
+        </Stack.Screen >
+
+        <Stack.Screen
+          name='ModuleAddScreen'
+          component={ModuleAddScreen}
+          options={{title: 'Add Modules'}}
+        >
+
+        </Stack.Screen>
+
+        <Stack.Screen
+          name='ModuleViewScreen'
+          component={ModuleViewScreen}
+          options={{title: 'View Modules'}}
+        >
+
+        </Stack.Screen>
+
+        <Stack.Screen
+          name='ModuleModifyScreen'
+          component={ModuleModifyScreen}
+          options={{title: ' Modules'}}
+        >
+
+        </Stack.Screen>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/screens/ModuleListScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text } from 'react-native';
+import Screen from '../layouts/Screen';
+
+const ModuleListScreen = () => {
+  return (
+    <Screen>
+      <Text>List</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default ModuleListScreen;
