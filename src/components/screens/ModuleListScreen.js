b3327ccddc9f924033179e7a692dffbac1567117
Michael <michaeljohn_charles@yahoo.com>
2024-10-01 22:40:23 +0100

    adding data

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,16 +1,35 @@
-import { StyleSheet, Text } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text } from 'react-native';
 import Screen from '../layouts/Screen';
+import initialModules from '../../data/modules';
 
 const ModuleListScreen = () => {
   return (
     <Screen>
-      <Text>List</Text>
+      <Pressable style={styles.container}>
+      {
+        initialModules.map((modules) => (
+          <View style={styles.item}>
+            <Text style={styles.text}>
+              {modules.ModuleCode} {modules.ModuleName}
+            </Text>
+          </View>
+        ))
+      }
+      </Pressable>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {},
+  item: {
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey',
+  },
+  text: {
+    fontSize: 16,
+  }
 });
 
 
