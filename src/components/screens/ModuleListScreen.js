4ef1e4a665f5157f15faa190d77914c6074046de
Michael <michaeljohn_charles@yahoo.com>
2024-10-04 00:41:15 +0100

    adding module view screen content and creating handlers

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,22 +1,39 @@
-import {ScrollView, StyleSheet } from 'react-native';
+import {ScrollView, StyleSheet, Text } from 'react-native';
 
 import Screen from '../layouts/Screen.js';
-import initialModules from '../../data/modules.js'
+import initialModules from '../../data/modules'
 import ModuleItem from '../../modules/ModuleItem.js'
+import { useState } from 'react';
+
+// console.log(initialModules[0].ModuleCode)
 
 
-const handleSelect = () => alert('Item selected');
+// const modules = initialModules;
 
 
-const ModuleListScreen = () => {
-  // const modules = initialModules;
+
+
+const ModuleListScreen = ({navigation}) => {
+  const [modules, setModules] = useState(initialModules);
+
+  
+  const handleSelect = (module) => navigation.navigate('ModuleViewScreen', { module })
+
+  const handleDelete = (module) => {
+    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+    console.log('Deleted ModuleID:', module.ModuleID);
+    
+  };
+
 
   return (
     <Screen>
       <ScrollView style={styles.container}>
       {
         initialModules.map((module) => (
-          <ModuleItem module={module} onSelect={handleSelect} />
+          // <ModuleItem module={module} onSelect={() => handleDelete(module)} />
+          <ModuleItem module={module} onSelect={() => handleSelect(module)} />
+
         ))
       }
       </ScrollView>
--- src/components/screens/ModuleViewScreen.js
+++ src/components/screens/ModuleViewScreen.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FullWidthImage from 'react-native-fullwidth-image';
 
-export default function ModuleViewScreen() {
+const ModuleViewScreen = ({navigate, route}) => {
+
+  const { module } = route.params;
+
+  if (!module) {
+    return <Text>Loading...</Text>;
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>View</Text>
-      <StatusBar style="auto" />
+      <FullWidthImage source={{uri:module.ModuleImage}} style={styles.image} />
+      <View style={styles.infoTray}>
+        <Text style={styles.boldText}>{module.ModuleCode} {module.ModuleName}</Text>
+        <Text style={styles.text}>Level {module.ModuleLevel}</Text>
+        <Text style={styles.text}>
+          {module.ModuleLeaderName} <Text style={styles.dimText}>(Module Leader)</Text>
+          </Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    gap: 15,
+    padding: 10
   },
+  infoTray: {
+    gap: 5
+  },
+  text: {
+    fontSize: 16
+  },
+  boldText: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  dimText: {
+    color: 'grey'
+  },
+  image: {
+    borderRadius: 3
+  }
 });
+
+export default ModuleViewScreen;
